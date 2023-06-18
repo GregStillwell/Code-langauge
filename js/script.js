596e204c753091980436cd56289d0d9cc72097f9
Greg <Jariye05@gmail.com>
2023-06-18 02:19:30 -0700

    this change a hidden javascript answer

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    const someInput = document.getElementById("question").value;
+
+
+
+
 function handleSelect(event) {
   event.preventDefault();
-  const selection = document.getElementById("petList").value;
+  const selection = document.getElementById("question").value;
 }
 
 window.addEventListener("load", function() {
-  document.getElementById("Pets").addEventListener("submit", handleSelect);
+  document.getElementById("codeLangauge").addEventListener("submit", handleSelect);
 });
 
 let catLover = document.getElementById("Javascript")
 catLover.setAttribute("class", "hidden");
+}
+}
 
