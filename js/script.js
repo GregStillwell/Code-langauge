f6b9829396cab44d77bcea7050fcb2a0232a33c9
Greg <Jariye05@gmail.com>
2023-06-24 15:35:42 -0700

    added a if branch

--- js/script.js
+++ js/script.js
@@ -7,14 +7,18 @@ window.onload = function() {
 
     const question = (document.getElementById("catLover").value);
     const question2 = (document.getElementById("favoriteColor").value);
+
     if (question === "javascript") {
-       const javascriptDiv = document.getElementById("javascript")
-       javascriptDiv.removeAttribute("class")
-    if (question ==="c#") {
-      const cDiv = document.getElementById("c#")
+      const javascriptDiv = document.getElementById("javascript")
+      javascriptDiv.removeAttribute("class")
+      
+
+
+    if (question === "C#") {
+      const cDiv = document.getElementById("C#")
       cDiv.removeAttribute("class")
       console.log(question)
-    };
+    }
     }
 
   }
