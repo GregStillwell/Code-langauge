e2128f38460bafb25e56d854be145cdcf69f2025
Greg <Jariye05@gmail.com>
2023-06-24 23:03:03 -0700

    wip

--- js/script.js
+++ js/script.js
@@ -3,32 +3,29 @@ window.onload = function() {
   form.onsubmit = function(event) {
     event.preventDefault();
 
-    const question1 = parseInt(document.querySelector("input[name='question1']:checked").value);
+    const question1 = parseInt(document.querySelector("input[name='question1]:checked").value);
     const question2 = parseInt(document.querySelector("input[name='question2']:checked").value);
     const question3 = parseInt(document.querySelector("input[name='question3']:checked").value);
     const question4 = parseInt(document.querySelector("input[name='question4']:checked").value);
     const question5 = parseInt(document.querySelector("input[name='question5']:checked").value);
     const result = question1 + question2 + question3 + question4 + question5;
+    
 
-    if (result >= 10) {
+    if (result >= 15) {
       Ruby.removeAttribute("class");
       form.setAttribute("class", "hidden");
       reset.removeAttribute("class");
-} else if (result >= 15) {
+      console.log(result)
+} else if (result >= 20) {
       javascript.removeAttribute("class");
       form.setAttribute("class", "hidden");
       reset.removeAttribute("class");
 
-} else if (result >= 20) {
+} else if (result >= 25) {
       cSharp.removeAttribute("class");
       form.setAttribute("class", "hidden");
       reset.removeAttribute("class");
 };     
-
-    document.getElementById("resetButton").addEventListener("click", (event)=> {
-    form.reset();
-    hideResults();
-    });
 
 
 
