d526fa3f3c8271f6dfa5efcfbc859db9266af5b5
Greg <Jariye05@gmail.com>
2023-06-25 01:37:40 -0700

    lowered the value numbers

--- js/script.js
+++ js/script.js
@@ -9,35 +9,37 @@ const question3 = parseInt(document.querySelector("input[name='question3']:check
 const question4 = parseInt(document.querySelector("input[name='question4']:checked").value);
 const question5 = parseInt(document.querySelector("input[name='question5']:checked").value);
 const result = question1 + question2 + question3 + question4 + question5;
-console.log(result)    
+  
 
-    if (result >= 13) {
+if (result >= 20) {
       ruby.removeAttribute("class");
       form.setAttribute("class", "hidden");
-      reset.removeAttribute("class");
+      
      
-} else if (result >= 20) {
+} else if (result >= 15) {
       javascript.removeAttribute("class");
       form.setAttribute("class", "hidden");
-      reset.removeAttribute("class");
+      console.log(result)
 
-} else if (result >= 25) {
+} else if (result >= 10) {
       cSharp.removeAttribute("class");
       form.setAttribute("class", "hidden");
-      reset.removeAttribute("class");
 
-      const javascript = document.getElementById("javascript");
-      const cSharp = document.getElementById("c#");
-      const ruby = document.getElementById("ruby");
+}
+
+      
+      
+
+const javascript = document.getElementById("javascript");
+const cSharp = document.getElementById("cSharp");
+const ruby = document.getElementById("ruby");
      
 };     
-   
+
 
 
     
     }
       
-
-    }
 
 
