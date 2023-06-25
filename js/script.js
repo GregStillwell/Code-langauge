2868d84a41290d08e37913775dc80e2a50acb05b
Greg <Jariye05@gmail.com>
2023-06-25 04:55:30 -0700

    reset button done

--- js/script.js
+++ js/script.js
@@ -3,52 +3,45 @@ window.onload = function() {
   form.onsubmit = function(event) {
     event.preventDefault(); 
 
-const question1 = parseInt(document.querySelector("input[name='question1']:checked").value);
+      const question1 = parseInt(document.querySelector("input[name='question1']:checked").value);
+      const question2 = parseInt(document.querySelector("input[name='question2']:checked").value);
+      const question3 = parseInt(document.querySelector("input[name='question3']:checked").value);
+      const question4 = parseInt(document.querySelector("input[name='question4']:checked").value);
+      const question5 = parseInt(document.querySelector("input[name='question5']:checked").value);
 
-const question2 = parseInt(document.querySelector("input[name='question2']:checked").value);
-
-const question3 = parseInt(document.querySelector("input[name='question3']:checked").value);
-
-const question4 = parseInt(document.querySelector("input[name='question4']:checked").value);
-
-const question5 = parseInt(document.querySelector("input[name='question5']:checked").value);
-
-const result = question1 + question2 + question3 + question4 + question5;
-
-
-const javascript = document.getElementById("javascript");
-const cSharp = document.getElementById("cSharp");
-const ruby = document.getElementById("ruby");
-const resetButton = document.getElementById("resetButton");
+      const result = question1 + question2 + question3 + question4 + question5;
 
 
 
-if (result >= 18) {
-      ruby.removeAttribute("class");
-      form.setAttribute("class", "hidden");
-      resetButton.removeAttribute("class");
-      
-     
-} else if (result >= 12) {
-      javascript.removeAttribute("class");
-      form.setAttribute("class", "hidden ");
-      resetButton.removeAttribute("class");
+      const javascript = document.getElementById("javascript");
+      const cSharp = document.getElementById("cSharp");
+      const ruby = document.getElementById("ruby");
+      const reset = document.getElementById("reset");
 
-} else if (result >= 6) {
-      cSharp.removeAttribute("class");
-      form.setAttribute("class", "hidden");
-      resetButton.removeAttribute("class");
 
-      let reset = document.querySelector("button#reset");
-      reset.addEventListener("click", function() {
-      window.location.reload()
+      if (result >= 15) {
+            ruby.removeAttribute("class");
+            form.setAttribute("class", "hidden");
+            reset.removeAttribute("class");
+      } else if (result >= 11) {
+            javascript.removeAttribute("class");
+            form.setAttribute("class", "hidden");
+            reset.removeAttribute("class");
+      }  else if (result >= 6) {
+            cSharp.removeAttribute("class");
+            form.setAttribute("class", "hidden");
+            reset.removeAttribute("class");
+
+
+      };
+      let resetButton = document.querySelector("button#reset");
+      resetButton.addEventListener("click", function() {
+      window.location.reload();
 
 });
-
-        
+  
 };
 
-      
-};   
+};
 
-}
+
