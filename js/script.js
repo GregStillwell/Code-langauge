2d7e587241d88b1ce5e2c0417141100235162575
Greg <Jariye05@gmail.com>
2023-06-25 02:26:10 -0700

    reset button

--- js/script.js
+++ js/script.js
@@ -4,42 +4,51 @@ window.onload = function() {
     event.preventDefault(); 
 
 const question1 = parseInt(document.querySelector("input[name='question1']:checked").value);
+
 const question2 = parseInt(document.querySelector("input[name='question2']:checked").value);
+
 const question3 = parseInt(document.querySelector("input[name='question3']:checked").value);
+
 const question4 = parseInt(document.querySelector("input[name='question4']:checked").value);
+
 const question5 = parseInt(document.querySelector("input[name='question5']:checked").value);
+
 const result = question1 + question2 + question3 + question4 + question5;
-  
 
-if (result >= 20) {
-      ruby.removeAttribute("class");
-      form.setAttribute("class", "hidden");
-      
-     
-} else if (result >= 15) {
-      javascript.removeAttribute("class");
-      form.setAttribute("class", "hidden");
-      console.log(result)
-
-} else if (result >= 10) {
-      cSharp.removeAttribute("class");
-      form.setAttribute("class", "hidden");
-
-}
-
-      
-      
 
 const javascript = document.getElementById("javascript");
 const cSharp = document.getElementById("cSharp");
 const ruby = document.getElementById("ruby");
      
+
+if (result >= 18) {
+      ruby.removeAttribute("class");
+      form.setAttribute("class", "hidden");
+      resetButton.removeAttribute("class");
+      
+     
+} else if (result >= 12) {
+      javascript.removeAttribute("class");
+      form.setAttribute("class", "hidden ");
+      resetButton.removeAttribute("class");
+
+} else if (result >= 6) {
+      cSharp.removeAttribute("class");
+      form.setAttribute("class", "hidden");
+      resetButton.removeAttribute("class");
+      console.log(resetBut)
+
+let resetButton = querySelector("resetButton");
+    resetButton.addEventListener("click", function() {
+        window.location.reload()
+        
+});
+
+      
 };     
 
 
 
     
     }
-      
-
-
+}
